7a9e7f764a26b38a5b63404e038031a60955db49
Leonardo Luz <leonardoluz10@hotmail.com>
2022-09-30 13:09:32 -0300

    Materiais didaticos para estudos na linguagens

---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// booleanos 
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 => false 
+// 1 => true
+
+/* console.log(0 == false); 
+console.log('' == false);
+console.log(1 == true); */
+
+
+// undefined ==> valor indefinido 
+// null ==> vazio ou nada
+let minhaVar;
+let varNull = null;
+ 
+//console.log(minhaVar);
+//console.log(varNull); 
+ 
+let numero = 3;
+let texto = "Alura";
+// typeof demostra o tipo de variavel que está sendo guardada na variavel
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
