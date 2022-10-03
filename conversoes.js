af3a0bd71f1ede207d1e492dd1d7438c80c1a878
Leonardo Luz <leonardoluz10@hotmail.com>
2022-10-03 11:38:51 -0300

    Tipos de conversoes

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado 
+// booleanos
+
+//  CONVERSÃO implicita 
+const numero =  456;
+const numeroString = Number('456a');
+
+
+// Number() para converter em numero  
+//String() para converter em string
+
+console.log(numero + numeroString);
+
+
+// conversão explicita
+
+
+
+
