cb89184818f73c6606d845442d74bdff278f8352
Leonardo Luz <leonardoluz10@hotmail.com>
2022-10-05 15:09:30 -0300

    Atualizando materia didatica

---
+++ comparacoes.js
@@ -0,0 +1,21 @@
+// === (comparações implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto);
+
+
+// typeof
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+
+// == só compara os valores 
+// === compara o valor e o tipo
+
+// boas praticas se uso conversão explicita com um uso de Number() e String() 
+
+
+
---
+++ exemplo1.js
@@ -0,0 +1,29 @@
+   
+/*    function cumprimentar(){
+    console.log('oi gente!')
+   }
+   
+   cumprimentar()
+
+   function cumprimentaPessoa(pessoa){
+    console.log(`oi, ${pessoa}!`)
+   }
+   
+   cumprimentaPessoa('Helena') */
+
+   function cumprimentar(){
+    return 'Oi gente!'
+   }
+   
+   function cumprimentaPessoa(nomePessoa) {
+    console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+   }
+   
+   cumprimentaPessoa('Paula') // “Oi gente! Meu nome é Paula”
+
+   function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   operacaoMatematica(15, 30, 45) // 90
+
