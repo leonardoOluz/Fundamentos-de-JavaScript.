80c2504184a1aa32589e0cd4a11d7b8febd668cd
Leonardo Luz <leonardoluz10@hotmail.com>
2022-10-10 12:55:27 -0300

    Complentado exercicios

--- JavaScript Arrays/dinner-today.js
+++ JavaScript Arrays/dinner-today.js
@@ -1,10 +1,12 @@
-jantarDeHoje = ['🍔', '🌭', '🍕']
+jantarDeHoje = ['🍔', '🌭', '🍕'] // lista de array com 3 elementos indice de 0 a 3.
+ 
+jantarDeHoje.pop() // retira o ultimo elemento 
+jantarDeHoje.pop() // retira o ultimo elemento
 
-jantarDeHoje.pop()
-jantarDeHoje.pop()
-
-jantarDeHoje.push('🍳')
-jantarDeHoje.push('🥗')
-jantarDeHoje.push('🍏')
+jantarDeHoje.push('🍳') // adiciona um novo elemento ao final da array
+jantarDeHoje.push('🥗') // adiciona um novo elemento ao final da array
+jantarDeHoje.push('🍏') // adiciona um novo elemento ao final da array
 
 console.log(jantarDeHoje);
+
+
