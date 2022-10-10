e85622d204495db405fd3ccc3e8b1dd0976e8709
Leonardo Luz <leonardoluz10@hotmail.com>
2022-10-10 09:00:40 -0300

    Reorganizando arquivos de fundamentos

--- README.md
+++
@@ -1,7 +0,0 @@
-# Fundamentos-de-JavaScript.
-
->Fundamentos para  Linguagem de JavaScript
-
-```
-Material de estudo para a liguagen de Java script 
-```
--- praticar.js
+++
@@ -1,4 +0,0 @@
-const soma = 10 + 10;
-const multiplicacao = 10 * 10;
-const operacao = ((soma + multiplicacao) + 10 / (multiplicacao * soma)^5);
-console.log(operacao);
