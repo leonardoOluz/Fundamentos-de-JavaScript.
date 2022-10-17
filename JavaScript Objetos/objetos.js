9eb3a01e739acc6e7666b90dadafc8cb8f84310a
Leonardo <leonardoluz10@hotmail.com>
2022-10-17 17:03:12 -0300

    Começando curso de JavaScript Objetos

---
+++ JavaScript Objetos/objetos.js
@@ -0,0 +1,14 @@
+// Lista de arrays -> const listaCPFs = ['123456789','123456789','12345678987']
+
+// Lista de arrays  -> const cliente = ["Ana","André","idade",36];
+
+
+// objetos
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "123456789",
+    email: "andre@email.com"
+}
+
+ 
