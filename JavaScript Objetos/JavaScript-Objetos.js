a3a04d98cdf5264dd4a88b64624f69e7805d00a3
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-03 10:57:54 -0300

    Adicionando objeto dentro de objeto

--- JavaScript Objetos/JavaScript-Objetos.js
+++ JavaScript Objetos/JavaScript-Objetos.js
@@ -1,9 +1,9 @@
 //      #  JavaScript Objetos  #
 
 
-const listaCpfs = ['123456798','123456465','123456456'] // uma array sozinha não é capaz de guardar dados necessario para um objeto
+const listaCpfs = ['123456798', '123456465', '123456456'] // uma array sozinha não é capaz de guardar dados necessario para um objeto
 
-const cliente = ["nome","André", "idade", 36]// com apenas array só podemos guardar os valores dentro
+const cliente = ["nome", "André", "idade", 36]// com apenas array só podemos guardar os valores dentro
 
 
 // * Criando um Objeto Cliente *
@@ -12,7 +12,7 @@ const cliente = ["nome","André", "idade", 36]// com apenas array só podemos gu
 
 //    objeto
 const cliente1 = {
-//  keys:  valores    
+    //  keys:  valores    
     nome: "André",
     idade: 36,
     cpf: "1232456789",
@@ -31,7 +31,7 @@ const cliente1 = {
 // *** Formas de acessar uma chave sem o nome da chave no objeto ***
 
 
-const chaves = ["nome","idade", "cpf", "email"]// usando uma array com os valores escrito com os nomes da chaves do objeto cliente1
+const chaves = ["nome", "idade", "cpf", "email"]// usando uma array com os valores escrito com os nomes da chaves do objeto cliente1
 
 // console.log(cliente1[chaves[0]]) // usando notação de conchetes para passar os valores da variavel chaves
 
@@ -56,7 +56,7 @@ cliente1.fone = "123456789"; // passando com anotação de ponto uma nova chave 
 
 console.log(cliente1)// depois de passar a nova chave e valor
 
-cliente1.fone ="321654987"; // Ao passarmos um novo valor a chave criada no objeto cliente1, estamos sobreescrevendo os valores dentro da chave.
+cliente1.fone = "321654987"; // Ao passarmos um novo valor a chave criada no objeto cliente1, estamos sobreescrevendo os valores dentro da chave.
 
 console.log(cliente1);// depois de sobrescrever o valores da chave imprindo verificamos que foi alterado os valores de .fone do cliente1
 
@@ -64,6 +64,40 @@ console.log(cliente1);// depois de sobrescrever o valores da chave imprindo veri
 
 //  ***** Adicionado mais de um valor dentro da chave *****
 
+const cliente2 = {
+//  keys:  valores    
+    nome: "André",// dados do tipo estring
+    idade: 36,// dados do tipo number
+    cpf: "1232456789",// dados do tipo string
+    email: "andre@email.com",// dados do tipo estring
+
+    // chave fone = dados do tipo array
+    fones: ["321654987", "3216549876"] // Para adicionar uma chave com valores dentro de array precisamos colocar conchetes [] depos da chave
+}
+
+cliente2.fones.forEach(fones => console.log(fones)) // usamos um metodo de array forEach para percorrer todos os valores da array fones do objeto cliente2
 
 
- 
+
+
+// ******    Adicionando objetos dentro de objetos    ******
+
+
+
+cliente2.dependentes = { // Ao colocarmos um objeto existente e passar uma nova chave com anotação de ponto e passando para essa chave = { as novas chaves com seus valores dentro} criamos um novo objeto dentro de outro objeto
+    nome:"Sara",
+    parentesco: "filha",
+    dataNasc: "20/03/2011"
+}
+
+console.log(cliente2); // imprimindo o objeto cliente2 com seu novo objeto dependentes
+
+
+
+//  Como fazer alteração dentro do objeto dependentes que está dentro do objeto cliente2
+
+
+
+cliente2.dependentes.nome = "Sara Silva";
+
+console.log(cliente2)
