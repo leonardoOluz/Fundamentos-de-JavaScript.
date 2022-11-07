77738dba1c179083edabf15d3e769c84449fdc86
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-07 13:04:18 -0300

    Tipos de conversões

--- fundamentos/fundamentos.js
+++ fundamentos/fundamentos.js
@@ -174,3 +174,54 @@ if (forma === 'quadrado') {
 
 console.log(area)
 
+//                #     Truthy or Falsy
+
+// booleanos 
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 => false 
+// 1 => true
+
+console.log(0 == false); 
+console.log('' == false);
+console.log(1 == true);
+
+
+// undefined ==> valor indefinido 
+// null ==> vazio ou nada
+let minhaVar;
+let varNull = null;
+ 
+//console.log(minhaVar);
+//console.log(varNull); 
+ 
+let numero = 3;
+let texto = "Alura";
+// typeof demostra o tipo de variavel que está sendo guardada na variavel
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
+
+//  CONVERSÃO implicita 
+
+// conversão implicita usando a comparacação de ( == ); -> comparando valores dentros das chaves e variaveis
+
+
+
+
+// Conversão explicita
+
+const numero1 =  456; 
+const numeroString = Number('456'); // utilizando os conversores antes de declarar a variavel
+//Number('456a'); usando uma letra dentro do numero, não é possivel fazer nenhum tipo de calculo pois a conversão não consegue converter
+
+// Number() para converter em numero  
+//String() para converter em string
+
+console.log(numero1 + numeroString);
+
+
