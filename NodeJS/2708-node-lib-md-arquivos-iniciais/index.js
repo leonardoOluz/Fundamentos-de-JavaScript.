ffc6c7b90a7b5ab4c067dd388b8ad8723f7fa123
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-09 18:22:13 -0300

    Aprendendo o uso do then()

--- NodeJS/2708-node-lib-md-arquivos-iniciais/index.js
+++ NodeJS/2708-node-lib-md-arquivos-iniciais/index.js
@@ -1,40 +1,49 @@
 import fs from 'fs';
-import chalk from 'chalk'; // Formato em uso para importar arquivos, libs e framework.
-
-function pegarArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) =>{
-        console.log(chalk.green(texto))
-    })
-}
-
-pegarArquivo('./arquivos/texto.md')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import chalk from 'chalk'; // Formato em uso para importar arquivos, libs e framework. usado apartir do ES6 ESM
 
 // const chalk = require('chalk'); a forma de importar outros arquivos usando o require está ficando em desuso. Entrando no lugar o formato de import nome da variavle from arquivo buscado.
 
 
 
-// console.log(chalk.blue('Hello world!'));
+
+function trataErro(erro) { // função erro demostrar no terminal o erro
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
 
 
-// console.log(chalk.yellow('olá mundo'));
 
-// console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
-// console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)');
+
+// Promises com then()
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+
+}
+
+
+//  ******     Função Sincrona     ******
+// function pegarArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>{
+//        if (erro) {
+//         trataErro(erro);// se caso aparecer erro chama a função
+//        }
+//         console.log(chalk.green(texto))
+//     })
+// }
+
+//pegarArquivo('./arquivos/texto.md')
+
+
+
+
+
+
+
+
+
