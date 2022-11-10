0f0023859d148f101eedd4cb80d5e3ad652553c9
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-10 10:42:00 -0300

    Refatorando

--- NodeJS/2708-node-lib-md-arquivos-iniciais/index.js
+++ NodeJS/2708-node-lib-md-arquivos-iniciais/index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs';// filesystem biblioteca nativa do Node
 import chalk from 'chalk'; // Formato em uso para importar arquivos, libs e framework. usado apartir do ES6 ESM
 
 // const chalk = require('chalk'); a forma de importar outros arquivos usando o require está ficando em desuso. Entrando no lugar o formato de import nome da variavle from arquivo buscado.
@@ -7,18 +7,23 @@ import chalk from 'chalk'; // Formato em uso para importar arquivos, libs e fram
 
 //   *****   função erro demostrar no terminal o erro   ******
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+    // uso do throw jogar para fora 
+    // new Error junto com o throw estamos lançando um objeto com um novo erro, demostrando o tipo de erro 
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));// usando o erro.code : codigo do erro, mais podemos ver o objeto todo para verificar o que pode ser utilizado.
 }
 
 
-//     *****    async/await   ****** 
+//     *****    async/await   código assincrono  ****** 
 async function pegaArquivo(caminhoDoArquivo) {
-    try {
+    // dentro de uma função assincrona podemos usar os blocos try/catch para que possamos tratar das funcionalidades
+    try {// try acontece tudo que queremos que aconteça caso de sucesso para receber o arquivo
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
         console.log(chalk.green(texto))
-    } catch(erro){
+    } catch (erro) {// catch recebe o erro podendo trabalhar com ele e usar outras função para tratar 
         trataErro(erro)
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
     }
 
 }
@@ -27,18 +32,18 @@ pegaArquivo('./arquivos/texto.md')
 pegaArquivo('./arquivos/')
 
 
-//  ******   Promises com then()   *****
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
+//  ******   Promises com then() código assincrono  *****
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
 
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch(trataErro)
-// }
+    fs.promises// Uso do fs.promises para passar ao javascript que está trabalhando com assincrona
+        .readFile(caminhoDoArquivo, encoding)// com o .readFile passamos o parâmetro que deve ser lido pelo metodo e seu segundo parâmentro para dizer que tipo de encode esta usando na leitura.
+        .then((texto) => console.log(chalk.green(texto)))// encadeamos a função de callback .then para que ela assim que receber o arquivo de texto pelo .readFile faça um console.log, acaso receba o arquivo obs: o then é uma função para trabalhar com promessas
+        .catch(trataErro)// o .catch tbm é uma função de callback preparada para pegar erros do tipo assincrona 
+}
 
 
-//  ******     Função Sincrona     ******
+// //  ******     Função Sincrona     ******
 // function pegarArquivo(caminhoDoArquivo){
 //     const encoding = 'utf-8';
 //     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>{
@@ -49,4 +54,4 @@ pegaArquivo('./arquivos/')
 //     })
 // }
 
-//pegarArquivo('./arquivos/texto.md')
+// pegarArquivo('./arquivos/texto.md')
