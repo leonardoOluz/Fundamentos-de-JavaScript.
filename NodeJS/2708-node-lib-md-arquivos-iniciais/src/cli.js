6f8b6d21f6010f2078d1128f063c8d0b92499c15
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-18 16:32:48 -0300

    Criando arquivo de validação de http

--- NodeJS/2708-node-lib-md-arquivos-iniciais/src/cli.js
+++ NodeJS/2708-node-lib-md-arquivos-iniciais/src/cli.js
@@ -1,20 +1,30 @@
 import pegaArquivo from './index.js';
 import fs from 'fs';
 import chalk from 'chalk';
+import listaValidada from './http-validacao.js';
 
 const caminho = process.argv; //O Node.js tem um objeto próprio, chamado objeto process.
 
-function imprimeLista(resultado, identificador = ''){
-    console.log(
-        chalk.yellow(`Lista de links`),
-        chalk.black.bgGreen(identificador),
-        resultado);
+function imprimeLista(valida, resultado, identificador = ''){
+    if (valida) {
+        console.log(
+            chalk.yellow(`Lista validada`),
+            chalk.black.bgGreen(identificador),
+           listaValidada(resultado));
+    }else {
+        console.log(
+            chalk.yellow(`Lista de links`),
+            chalk.black.bgGreen(identificador),
+            resultado);
+    }
+
 }
 
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
-
+    const valida = argumentos[3] === '--valida';
+    
     // Verificando se há um caminho ou diretorio 
     try {
         fs.lstatSync(caminho);
@@ -25,18 +35,18 @@ async function processaTexto(argumentos) {
         }
     }
     // condições composta para verificar se é arquivo ou diretorio
-    if (fs.lstatSync(caminho).isFile()) {// tratando um arquivo
+    if (fs.lstatSync(caminho).isFile()) {// tratando um arquivo se for arquivo entra com true
         
         const resultado = await pegaArquivo(argumentos[2]);// pegarArquivo retorna uma promessa porisso o uso do async/await
-        imprimeLista(resultado)
+        imprimeLista(valida,resultado)
 
-    } else if (fs.lstatSync(caminho).isDirectory()) {// tratando um diretorio
+    } else if (fs.lstatSync(caminho).isDirectory()) {// tratando um diretorio se for diretorio entra com true
         
         const arquivos = await fs.promises.readdir(caminho)
        // passamos para o forEach que estamos trabalhando com promessas passando o async/await
         arquivos.forEach( async(nomeDeArquivo) => { // usando um forEach para percorrer cada arquivo dentro do diretorio
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-          imprimeLista(lista, nomeDeArquivo)
+          imprimeLista(valida, lista, nomeDeArquivo)
         
         })        
     }       
---
+++ NodeJS/2708-node-lib-md-arquivos-iniciais/src/http-validacao.js
@@ -0,0 +1,5 @@
+export default function listaValidada(listaDelinks){
+    return 'entrou na função'
+
+}
+
