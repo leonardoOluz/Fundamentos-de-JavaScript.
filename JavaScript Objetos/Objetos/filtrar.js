e886455cd06cb403d7513fca34874deaa7c90eb5
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-23 15:25:01 -0300

    Trabalhando com o uso do filter

--- JavaScript Objetos/Objetos/filtrar.js
+++ JavaScript Objetos/Objetos/filtrar.js
@@ -1,11 +1,12 @@
-const clientes = require('./cliente.json')
+const clientes = require('./cliente.json');
 
+// Trabalhando com o uso do filter para filtrar uma lista de objetos conforme os parâmentros passados
 function clienteSemComplementoAp(clientes) {
     return clientes.filter((cliente) => {
         return (cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty('complemento'))
     })
 }
 
-const listaSemCompl = clienteSemComplementoAp(clientes)
-console.log(listaSemCompl)
+const clienteSemCompl = clienteSemComplementoAp(clientes)
+console.log(clienteSemCompl)
 
