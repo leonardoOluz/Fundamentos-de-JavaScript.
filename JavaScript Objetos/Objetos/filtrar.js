af4da12b298094902e2bbe55a5b44bf4e106a057
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-23 12:03:19 -0300

    Filtrando clientes com Ap ou não.

--- JavaScript Objetos/Objetos/filtrar.js
+++ JavaScript Objetos/Objetos/filtrar.js
@@ -1,7 +1,25 @@
 const clientes = require('./cliente.json')
 
-function filtrarApartamentoSemComplemento(clientes) {
-    return clientes.filter((cliente)=> {
-        return
+function filterApNotComplet(clientes) {
+    return clientes.filter((cliente) => {
+        return (cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty('complemento'))
+    });
+}
+
+function filterNoAp(clientes) {
+    return clientes.filter((cliente) => {
+        return (!cliente.endereco.apartamento)
     })
 }
+function filterApOk(clientes) {
+    return clientes.filter((cliente) => {
+        return (cliente.endereco.apartamento && cliente.endereco.hasOwnProperty('complemento'))
+    })
+}
+
+const filtrados = filterApNotComplet(clientes)
+const filtradosNoAp = filterNoAp(clientes)
+const filtradosYesAp = filterApOk(clientes)
+
+console.log(filtradosYesAp)
+
