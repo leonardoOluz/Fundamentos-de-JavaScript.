697bb01c668925a558b4b25bb347d51de6c246a4
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-05 12:34:55 -0300

    Constancia em ordenar filtrar e pesquisar Objs

--- JavaScript Objetos/Objetos/Ordenar.js
+++ JavaScript Objetos/Objetos/Ordenar.js
@@ -1,24 +1,10 @@
-const clientes = require('./cliente.json');
+const cliente = require('./cliente.json')
 
-function ordenar(lista, propriedade, ordem = 'crescente'){
-    const resultados = lista.sort((a,b) => {
-        if(a[propriedade] < b[propriedade]){
-            return -1;
-        }
-        if(a[propriedade] > b[propriedade]){
-            return 1;
-        }
-        return 0;
-    })
-    if(ordem !== 'crescente'){
-        resultados.reverse();
-    }
-    return resultados
+function pesquisarCliente(objLista, chave, valor){
+    return objLista.find((lista) => lista[chave].includes(valor))
 }
-const listOrdenada = ordenar(clientes, 'nome')
-// ao trabalharmos com objetos qualquer alteração, sempre estamos alterando a origem 
-
-// const listDecrescente = ordenar(clientes, 'nome', 1)
 
 
-console.log(listOrdenada)
+const clienteResult = pesquisarCliente(cliente, 'nome', 'Vania')
+
+console.log(clienteResult)
