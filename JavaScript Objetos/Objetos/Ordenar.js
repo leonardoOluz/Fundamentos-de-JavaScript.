39ee2ee746d427feea1aa5977016f005e1e00094
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-07 16:12:58 -0300

    Criando lista ordenada

--- JavaScript Objetos/Objetos/Ordenar.js
+++ JavaScript Objetos/Objetos/Ordenar.js
@@ -1,10 +1,21 @@
 const cliente = require('./cliente.json')
 
-function pesquisarCliente(objLista, chave, valor){
-    return objLista.find((lista) => lista[chave].includes(valor))
+function ordenar(cliente, propriedade, decrescente = 'crescente') {
+    const resultados = cliente.sort((a, b) => {
+        if (a[propriedade] < b[propriedade]) {
+            return -1
+        } else if (a[propriedade] > b[propriedade]) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+    if(decrescente !== 'crescente'){
+        resultados.reverse();
+    }
+    return resultados
 }
 
+const nomeReverso = ordenar(cliente, 'nome', 1)
 
-const clienteResult = pesquisarCliente(cliente, 'nome', 'Vania')
-
-console.log(clienteResult)
+console.log(nomeReverso)
